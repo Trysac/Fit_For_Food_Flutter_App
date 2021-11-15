60065d4bae352ecc85a5361c9b7def41da6ad22e
Trysac <jmgomezsac@gmail.com>
2021-11-15 13:20:42 -0500

    Creación de Bean's - API Login

---
+++ APIs/Fit_For_Food_Login/src/main/java/com/UTP/Integrador_2/Fit_For_Food_Login/beans/UsuarioBean.java
@@ -0,0 +1,18 @@
+package com.UTP.Integrador_2.Fit_For_Food_Login.beans;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class UsuarioBean {
+    private Long id;
+    private String correo;
+    private String contraseña;
+    private Date fechaCreacion;
+    private Date fechaUltimoInicioSesion;
+    private String codigoSesion;
+    private String fechaUltimoCambioContraseña;
+}
---
+++ APIs/Fit_For_Food_Login/src/main/java/com/UTP/Integrador_2/Fit_For_Food_Login/beans/UsuarioPremiumBean.java
@@ -0,0 +1,17 @@
+package com.UTP.Integrador_2.Fit_For_Food_Login.beans;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class UsuarioPremiumBean {
+    private int id;
+    private int idUsuario;
+    private Date fechaRenovacion;
+    private Date fechaUltimaSuscripcion;
+    private String historicoSuscripcion;
+    private byte estadoSuscripcion;
+}
