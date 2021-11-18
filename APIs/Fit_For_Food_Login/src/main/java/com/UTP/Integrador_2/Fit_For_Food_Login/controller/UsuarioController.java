2386294287bb3e92e0e9949c0f56d6593b141f47
Trysac <jmgomezsac@gmail.com>
2021-11-18 18:14:35 -0500

    Correccion controlador usuario - Login

--- APIs/Fit_For_Food_Login/src/main/java/com/UTP/Integrador_2/Fit_For_Food_Login/controller/UsuarioController.java
+++ APIs/Fit_For_Food_Login/src/main/java/com/UTP/Integrador_2/Fit_For_Food_Login/controller/UsuarioController.java
@@ -1,6 +1,5 @@
 package com.UTP.Integrador_2.Fit_For_Food_Login.controller;
 
-import com.UTP.Integrador_2.Fit_For_Food_Login.beans.UsuarioBean;
 import com.UTP.Integrador_2.Fit_For_Food_Login.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +11,6 @@ public class UsuarioController {
 
     @Autowired
     UsuarioService usuarioService;
-
-//    @GetMapping("/usuario/{userID}")
-//    public UsuarioBean getRegisterByUserID(@PathVariable int userID) {
-//        UsuarioBean bean = this.usuarioService.getByUserID(userID);
-//        return bean;
-//    }
 
     @GetMapping("/usuario/{userCredencials}")
     public String validateUser(@PathVariable Map<String, String> userCredencials) {
